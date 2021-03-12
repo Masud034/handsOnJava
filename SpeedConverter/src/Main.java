c842be0a62e580f6f0a6290e74ea65e9605d0443
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-03-12 16:28:29 +0600

    Add files via upload

---
+++ SpeedConverter/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SpeedConverter s1 = new SpeedConverter();
+        s1.printConversion(1.5);
+        s1.printConversion(10.25);
+    }
+}
