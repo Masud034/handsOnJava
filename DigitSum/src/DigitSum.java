9cd585ec5e56ca543fa85509c08c8c038a03be26
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-04-20 02:53:47 +0600

    Add files via upload

---
+++ DigitSum/src/DigitSum.java
@@ -0,0 +1,22 @@
+public class DigitSum {
+    public static void main(String[] args) {
+
+        System.out.println(sumDigits(123));
+        System.out.println(sumDigits(1111));
+        System.out.println(sumDigits(9));
+        System.out.println(sumDigits(-123));
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        } else {
+            int sum = 0;
+            while (number > 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
