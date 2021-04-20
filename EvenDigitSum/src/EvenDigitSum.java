413d3b4ee757f625fc6e275429bf811c60ab2386
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-04-21 00:26:28 +0600

    Add files via upload

---
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -0,0 +1,29 @@
+public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(1234));
+        System.out.println(getEvenDigitSum(1234));
+        System.out.println(getEvenDigitSum(1234));
+        System.out.println(getEvenDigitSum(1234));
+
+
+    }
+
+    public static int getEvenDigitSum(int number) {
+        int sum = 0;
+        int isEven;
+
+        if (number < 0) {
+            return -1;
+        }
+        while (number > 0) {
+            isEven = number % 10;
+            number = number / 10;
+
+            if (isEven % 2 == 0) {
+                sum += isEven;
+            }
+        }
+        return sum;
+
+    }
+}
