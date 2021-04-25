2a9afac06f52cd1324d1176f232c7c5673be1407
Mohammad Masudul Alam <malam172034@bscse.uiu.ac.bd>
2021-04-25 17:04:10 +0600

    Add files via upload

---
+++ Point/src/Point.java
@@ -0,0 +1,43 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point (){
+
+    }
+
+    public Point(int x, int y){
+        setX(x);
+        setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(){
+        return Math.sqrt(Math.pow(getX(),2) +  Math.pow(getY(), 2));
+    }
+
+    public double distance(int x, int y){
+        return Math.sqrt(Math.pow(getX() - x, 2) + Math.pow(getY() - y, 2));
+    }
+
+    public double distance(Point point){
+        return distance(point.x, point.y);
+    }
+
+
+}
